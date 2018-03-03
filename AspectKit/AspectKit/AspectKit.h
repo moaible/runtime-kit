193b827170d6ab91fb867b39f59a86914c5d17e7
moaible <moai.motodera@gmail.com>
2018-03-03 17:18:19 +0900

    feat: Add new project files

---
+++ AspectKit/AspectKit/AspectKit.h
@@ -0,0 +1,16 @@
+// 
+// Copyright (c) moaparts.
+// All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AspectKit.
+FOUNDATION_EXPORT double AspectKitVersionNumber;
+
+//! Project version string for AspectKit.
+FOUNDATION_EXPORT const unsigned char AspectKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AspectKit/PublicHeader.h>
+
+
